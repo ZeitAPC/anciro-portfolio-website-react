fc7e8307ab3fa82cc64239ed5f4e3a1fbbfd72a4
ZeitAPC <fdanciro@student.apc.edu.ph>
2025-12-18 15:30:30 +0800

    changed package homepage

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
 )
